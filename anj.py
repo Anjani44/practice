3ad4531c04510c52b70529359abdbe316552ecfd
Anjani44 <anjaniyt5@gmail.com>
2025-09-15 21:38:34 +0530

    Add files via upload
    added one file

---
+++ anj.py
@@ -0,0 +1,39 @@
+print("hello")
+print(45+33-78*4/2)
+print(" i am happy ")
+Name = "Anjani"
+age = 18
+w = 12.340
+t = 12.0048345678
+u = 'true'
+print("My name is ", Name)
+print(type(Name))
+print(type(age))
+print(type(w))
+print(type(t))
+print(type(u))
+name1 = 'sk'
+name2 = "sk"
+name3 = '''sk'''
+print(name1)
+print(name2)
+print(name3)
+old = True         #write True and False and None in capital letters otherwise it will show an error.
+tmkoc = None
+print(type(old))       
+print(type(tmkoc))
+h = "anjani"
+o = "Anjani"
+print(h)
+a = 2
+b = 4
+sum = a + b
+print(sum)
+
+
+
+
+
+
+
+
